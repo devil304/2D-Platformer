ce127ea66cf0eeac4eed30f5a65d9d8ed60e20c9
Szymon Rzosinski <szymonrzosinski@gmail.com>
2021-07-08 23:04:45 +0200

    Jumping WIP

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -15,7 +15,9 @@ public class Movement : MonoBehaviour
     [SerializeField] MainIput MainInputAsset;
     float HorizontalDir = 0;
     bool jump = false;
-    [SerializeField] float Speed = 1, SpeedInAir = 0.5f;
+    [SerializeField] float JumpMaxTime = 2f;
+    float jumpTime = 0f;
+    [SerializeField] float Speed = 1, SpeedInAir = 0.5f, JumpSpeed = 7.5f, fallingSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +29,15 @@ public class Movement : MonoBehaviour
         MainInputAsset.Main.Jump.performed += perf => {
             if(perf.phase == InputActionPhase.Performed)
             {
-                jump = perf.ReadValueAsButton();
+                if(grounded)
+                    jump = true;
             }  
+        };
+        MainInputAsset.Main.Jump.canceled += canc => {
+            if (canc.phase == InputActionPhase.Canceled)
+            {
+                jump = false;
+            }
         };
         MainInputAsset.Main.Hotizontal.performed += perf => {
             if (perf.phase == InputActionPhase.Performed)
@@ -50,10 +59,17 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (jump && jumpTime < JumpMaxTime)
+        {
+            jumpTime += Time.deltaTime;
+        }
+        else
+            jump = false;
         RaycastHit2D hit = Physics2D.BoxCast((Vector2)transform.position-(Vector2.up*(MyBoxCollider2D.size.y/4)), MyBoxCollider2D.size*0.9f,0,Vector2.down,1f,LayerMask.GetMask("Default"));
         if(hit && hit.collider.gameObject.tag == "Ground")
         {
             grounded = true;
+            jumpTime = 0;
         }
         else
         {
@@ -61,6 +77,8 @@ public class Movement : MonoBehaviour
             grounded = false;
         }
         Vector2 velocity = MyRigidbody2D.velocity;
+        if (jump)
+            velocity.y = JumpSpeed;
         if (!grounded)
         {
             if (MyRigidbody2D.velocity.y > 0)
@@ -70,6 +88,10 @@ public class Movement : MonoBehaviour
             else
             {
                 MyAnimator.SetInteger("VerticalD", -1);
+            }
+            if (!jump)
+            {
+                velocity.y = -fallingSpeed;
             }
         }
         else
